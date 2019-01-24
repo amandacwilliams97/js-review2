34f8b8ee41e9a484116e5aabefa15913d9a4fece
Amanda Williams <amandacwilliams97@gmail.com>
2019-01-24 13:03:57 -0500

    added funciton instructions

---
+++ js-review2.js
@@ -0,0 +1,29 @@
+/*
+    Amanda Williams
+    January 24, 2019
+    328/js-review2.js
+ */
+/*
+The == operator compares objects by identity.
+ But sometimes you’d rather compare the
+ values of their actual properties. Write a
+ function objEquals that takes two objects
+ and returns true only if they have the same
+  properties and property values. The
+  Object.keys function might be useful!
+  Test your function thoroughly.
+ */
+function objEquals() {
+
+}
+
+/*
+Write a method flatten() can take an
+array of arrays and return a single
+array that has all the elements of the
+original arrays. You should utilize the
+reduce() method and the concat() method.
+ */
+function flatten(arr) {
+
+}
